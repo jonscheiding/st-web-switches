cce9add904fc6925891b505f5999f4807cdef790
Jonathan Scheiding <jscheiding@academicbenchmarks.com>
2016-02-20 12:20:56 -0500

    Simple Hello, World! - HTML edition

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
-app.get("/", function(req, res) {
-  res.json({message: "Help, I'm alive."});
-});
+var webroot = path.join(__dirname, "htdocs");
+var options = {
+  index: "index.html"
+};
+
+app.get("/", express.static(webroot, options));
 
 app.listen(3000);
