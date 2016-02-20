b2fe0f8bb057da5942ba9ba3f28c32b3a6890148
Jon Scheiding <jonscheiding@gmail.com>
2016-02-20 12:43:08 -0500

    Implemented OAuth2 flow

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var path = require("path");
+var oauth2 = require("simple-oauth2")({
+  clientID: "5db657c1-4b11-4b01-b1b9-ce92f3eb37e4",
+  clientSecret: "bd3da063-5586-430a-9a09-d1dee76e58dd",
+  site: "https://graph.api.smartthings.com/",
+  authorizationPath: "/oauth/authorize",
+  tokenPath: "/oauth/token"
+});
 
 var app = express();
 var auth_token = null;
+var auth_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: "http://localhost:3000/authorize/callback",
+  scope: "app",
+  state: "&h51H~"
+});
 
 var webroot = path.join(__dirname, "htdocs");
 var options = {
@@ -10,6 +22,20 @@ var options = {
 };
 
 app.get("/", express.static(webroot, options));
+
+app.get("/authorize", function(req, res) {res.redirect(auth_uri);});
+app.get("/authorize/callback", function(req, res) {
+  var code = req.query.code;
+  
+  oauth2.authCode.getToken({
+    code: code,
+    redirect_uri: "http://localhost:3000/authorize/callback"
+  }, function(error, result) {
+    auth_token = oauth2.accessToken.create(result);
+    console.log(auth_token);
+    res.redirect("/");
+  })
+})
 
 app.use("/api", function(req, res, next) {
   if(!auth_token) {
