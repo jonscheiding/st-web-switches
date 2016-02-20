bc6bc11d00e25b33f2f479ca57449147d73e2107
Jon Scheiding <jonscheiding@gmail.com>
2016-02-20 12:31:43 -0500

    Implemented 403's for API when not authenticated to SmartThings.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
+var auth_token = null;
 
 var webroot = path.join(__dirname, "htdocs");
 var options = {
@@ -9,5 +10,20 @@ var options = {
 };
 
 app.get("/", express.static(webroot, options));
+
+app.use("/api", function(req, res, next) {
+  if(!auth_token) {
+    res.status(403).json({
+      message: "Server is not authenticated to SmartThings."
+    });
+    return;
+  }
+  
+  next();
+});
+
+app.get("/api/heaters", function(req, res) {
+  res.send("You did it!");
+});
 
 app.listen(3000);
