e1f491d9fe5aab2b5003d0b388da2033dddc0308
Jonathan Scheiding <jscheiding@academicbenchmarks.com>
2016-02-20 13:11:43 -0500

    Talking to SmartThings API with Unireset

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var unirest = require("unirest");
 var oauth2 = require("simple-oauth2")({
   clientID: "5db657c1-4b11-4b01-b1b9-ce92f3eb37e4",
   clientSecret: "bd3da063-5586-430a-9a09-d1dee76e58dd",
@@ -9,7 +10,13 @@ var oauth2 = require("simple-oauth2")({
 });
 
 var app = express();
+// var auth_token = oauth2.accessToken.create({
+//   access_token: '9e4eb31a-8fb2-43ed-a6dc-9b9b3b34082a',
+//    token_type: 'bearer',
+//    expires_in: 1576711955  
+// });
 var auth_token = null;
+
 var auth_uri = oauth2.authCode.authorizeURL({
   redirect_uri: "http://localhost:3000/authorize/callback",
   scope: "app",
@@ -32,8 +39,8 @@ app.get("/authorize/callback", function(req, res) {
     redirect_uri: "http://localhost:3000/authorize/callback"
   }, function(error, result) {
     auth_token = oauth2.accessToken.create(result);
-    console.log(auth_token);
-    res.redirect("/");
+  
+    res.redirect("/api");
   })
 })
 
@@ -46,6 +53,14 @@ app.use("/api", function(req, res, next) {
   }
   
   next();
+});
+
+app.get("/api", function(req, res) {
+  unirest.get("https://graph.api.smartthings.com/api/smartapps/endpoints")
+     .headers({"Authorization": "Bearer " + auth_token.token.access_token})
+    .end(function(response) {
+      res.send(response.body);
+    });  
 });
 
 app.get("/api/heaters", function(req, res) {
