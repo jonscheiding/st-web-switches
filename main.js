2264e41c180c6b5c0c61e6ef322d772c04c5cfc0
Jon Scheiding <jonscheiding@gmail.com>
2016-02-20 19:46:15 -0500

    Refactored SmartThings authorization stuff into a separate class

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
 var express = require("express");
 var path = require("path");
 var unirest = require("unirest");
-var oauth2 = require("simple-oauth2")({
-  clientID: "5db657c1-4b11-4b01-b1b9-ce92f3eb37e4",
-  clientSecret: "bd3da063-5586-430a-9a09-d1dee76e58dd",
-  site: "https://graph.api.smartthings.com/",
-  authorizationPath: "/oauth/authorize",
-  tokenPath: "/oauth/token"
-});
+var stAuth = require("./lib/st-auth.js");
 
 var app = express();
-// var auth_token = oauth2.accessToken.create({
-//   access_token: '9e4eb31a-8fb2-43ed-a6dc-9b9b3b34082a',
-//    token_type: 'bearer',
-//    expires_in: 1576711955  
-// });
-var auth_token = null;
-
-var auth_uri = oauth2.authCode.authorizeURL({
-  redirect_uri: "http://localhost:3000/authorize/callback",
-  scope: "app",
-  state: "&h51H~"
-});
 
 var webroot = path.join(__dirname, "htdocs");
 var options = {
@@ -30,22 +12,13 @@ var options = {
 
 app.get("/", express.static(webroot, options));
 
-app.get("/authorize", function(req, res) {res.redirect(auth_uri);});
-app.get("/authorize/callback", function(req, res) {
-  var code = req.query.code;
-  
-  oauth2.authCode.getToken({
-    code: code,
-    redirect_uri: "http://localhost:3000/authorize/callback"
-  }, function(error, result) {
-    auth_token = oauth2.accessToken.create(result);
-  
-    res.redirect("/api");
-  })
-})
+app.get("/authorize", stAuth.authorizeHandler);
+app.get("/authorize/callback", stAuth.authorizeHandler.callback, function(req, res) {  
+  res.redirect("/api");
+});
 
 app.use("/api", function(req, res, next) {
-  if(!auth_token) {
+  if(!stAuth.getAuthToken()) {
     res.status(403).json({
       message: "Server is not authenticated to SmartThings."
     });
@@ -57,7 +30,7 @@ app.use("/api", function(req, res, next) {
 
 app.get("/api", function(req, res) {
   unirest.get("https://graph.api.smartthings.com/api/smartapps/endpoints")
-     .headers({"Authorization": "Bearer " + auth_token.token.access_token})
+     .headers({"Authorization": "Bearer " + stAuth.getAuthToken()})
     .end(function(response) {
       res.send(response.body);
     });  
