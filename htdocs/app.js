e97646f37c09e9a3da329bfe16f669c315b049f9
Jon Scheiding <jonscheiding@gmail.com>
2016-02-23 00:51:50 -0500

    Implemented on/off

--- htdocs/app.js
+++ htdocs/app.js
@@ -3,5 +3,15 @@ var app = angular.module("app", []);
 app.controller("AppController", function($scope, $http) {
   $http.get("/api/switches").then(function(response) {
     $scope.switches = response.data;
-  })
+  });
+  
+  $scope.toggle = function() {
+    var $scope = this;
+    var url = $scope.switch.links[$scope.switch.state == "off" ? "on" : "off"];
+    $http.put(url).then(function(response) {
+      $http.get($scope.switch.links.self).then(function(response) {
+        $scope.switch = response.data;
+      })
+    })
+  }
 });
