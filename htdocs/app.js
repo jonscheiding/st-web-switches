c76628ab007699ce86fe6806aa355a3ef9a7940c
Jonathan Scheiding <jscheiding@academicbenchmarks.com>
2016-02-23 01:15:31 -0500

    Implemented messages for loading and changing state.  Also some appearance tweaks

--- htdocs/app.js
+++ htdocs/app.js
@@ -1,17 +1,28 @@
 var app = angular.module("app", []);
 
 app.controller("AppController", function($scope, $http) {
+  $scope.loading = true;
+  
   $http.get("/api/switches").then(function(response) {
+    $scope.loading = false;
     $scope.switches = response.data;
   });
   
   $scope.toggle = function() {
-    var $scope = this;
-    var url = $scope.switch.links[$scope.switch.state == "off" ? "on" : "off"];
+    var $this = this;
+    var newState = $this.switch.state == "off" ? "on" : "off";
+    
+    $scope.changingSwitch = {
+      label: $this.switch.label,
+      to: newState
+    };
+    
+    var url = $this.switch.links[newState];
     $http.put(url).then(function(response) {
-      $http.get($scope.switch.links.self).then(function(response) {
-        $scope.switch = response.data;
-      })
-    })
+      $http.get($this.switch.links.self).then(function(response) {
+        $scope.changingSwitch = null;
+        $this.switch = response.data;
+      });
+    });
   }
 });
