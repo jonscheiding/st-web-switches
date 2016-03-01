a166b0d894d76dfa0a7df1c9e758a5cf395e8108
Jon Scheiding <jonscheiding@gmail.com>
2016-03-01 11:04:42 -0500

    Comments and readability refactors

--- htdocs/switch-app.js
+++ htdocs/switch-app.js
@@ -16,6 +16,9 @@ var app = angular.module("switch-app", [])
 app.controller("SwitchAppController", function($scope, $http, $window, $interval, $timeout) {
   $scope.loading = true;
   
+  //
+  // Refreshes the state of all switches
+  //
   $scope.reload = function() {
     $http.get($scope.api.links.switches).then(function(response) {
       $scope.loading = false;
@@ -23,10 +26,10 @@ app.controller("SwitchAppController", function($scope, $http, $window, $interval
     });
   };
   
-  $scope.authorize = function() {
-    $window.location.href = "/authorize";
-  };
-  
+  //
+  // Gets a description of when a switch will be turning off relative to now.
+  // E.G. "in an hour"
+  //
   $scope.switchOffWhen = function() {
     if(this.switch.state.is != "on" || !this.switch.state.until) {
       return;
@@ -35,6 +38,11 @@ app.controller("SwitchAppController", function($scope, $http, $window, $interval
     return moment(this.switch.state.until).fromNow();
   }
   
+  //
+  // Builds the message for the state bar.  Takes a message to display when
+  // the switch is unplugged, and a message to describe when it's turning off.
+  // Puts them together with a comma if they both need to be displayed.
+  //
   $scope.getStateMessage = function(displayParts) {
     var parts = [];
     if(this.switch.state.unplugged) {
@@ -48,6 +56,10 @@ app.controller("SwitchAppController", function($scope, $http, $window, $interval
     return message.firstLetterToUpperCase();
   }
   
+  //
+  // Makes an API call to toggle the switch on/off from its current state of
+  // off/on.
+  //
   $scope.toggle = function() {
     var $this = this;
     var newState = $this.switch.state.is == "off" ? "on" : "off";
@@ -66,6 +78,17 @@ app.controller("SwitchAppController", function($scope, $http, $window, $interval
     });
   }
   
+  //
+  // Redirects to authorization process
+  //
+  $scope.authorize = function() {
+    $window.location.href = "/authorize";
+  };
+  
+  //
+  // Start everything up by calling the API root so we can get our app info
+  // and links to other actions.
+  //
   $http.get("/api").then(function(response) {
     $scope.api = response.data;
     $scope.reload();
