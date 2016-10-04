6006c53666baa9a86fd1c20844cc27f0fac730ed
Jon Scheiding <jonscheiding@gmail.com>
2016-10-03 23:04:38 -0400

    #44 Created separate 'npm start' actions to handle auth for FNLLC

--- src/auth/fnllc.js
+++ src/auth/fnllc.js
@@ -1,15 +1,23 @@
+import envalid from 'envalid'
 import express from 'express'
 import expressSession from 'express-session'
 import ensureLogin from 'connect-ensure-login'
 import passport from 'passport'
 import OAuth2Strategy from 'passport-oauth2'
 
+envalid.validate(process.env, {
+  SESSION_SECRET: { required: true },
+  SM_OAUTH_ID: { required: true },
+  SM_OAUTH_SECRET: { required: true },
+  SM_API_URL: { required: true }
+})
+
 passport.serializeUser((user, done) => done(null, user))
 passport.deserializeUser((user, done) => done(null, user))
 
-passport.use(new OAuth2Strategy({
-  authorizationURL: 'http://schedulemaster-api.herokuapp.com/oauth2/authorize',
-  tokenURL: 'http://schedulemaster-api.herokuapp.com/oauth2/token',
+passport.use('fnllc', new OAuth2Strategy({
+  authorizationURL: process.env.SM_API_URL + '/oauth2/authorize',
+  tokenURL: process.env.SM_API_URL + '/oauth2/token',
   clientID: process.env.SM_OAUTH_ID,
   clientSecret: process.env.SM_OAUTH_SECRET,
   callbackURL: '/login',
@@ -21,10 +29,14 @@ passport.use(new OAuth2Strategy({
 ))
 
 var callback = express.Router()
-callback.get('/login', passport.authenticate('oauth2', { failureRedirect: '/', successRedirect: '/' }))
+callback.get('/login', passport.authenticate('fnllc', { failureRedirect: '/', successRedirect: '/' }))
 
-export default [
-  expressSession({resave: false, saveUninitialized: true, secret: 'TODO'}),
+export default () => [
+  expressSession({
+    resave: false, saveUninitialized: true, 
+    secret: process.env.SESSION_SECRET, 
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+  }),
   passport.initialize(),
   passport.session(),
   callback,
--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ import api from './api'
 import ui from './ui'
 
 const app = express()
+
+if(process.env.EXPRESS_AUTH) {
+  const auth = require('src/auth/' + process.env.EXPRESS_AUTH).default
+  app.use(auth())
+}
+
 app.use('/api', api)
 app.use('/', ui)
 
